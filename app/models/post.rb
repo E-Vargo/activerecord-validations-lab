c0abbc2b26d61ead89dd5958257c92dd2550e72c
E-Vargo <vargoelliott@gmail.com>
2022-01-25 15:39:11 -0800

    commit tree 997e1ef4bfbcb835d4bea687927af72f0328b468

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,19 @@
 class Post < ActiveRecord::Base
-end
+    #include ActiveModel::Validations
+
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    validate :clickbait?
+
+    PATTERNS = [
+      /Won't Believe/i, /Secret/i, /Top [number]/i, /Guess/i
+    ]
+  
+    def clickbait?
+      if PATTERNS.none? { |p| p.match title }
+      errors[:title] << "Not click-batey enough!!"
+      end
+    end
+  end
